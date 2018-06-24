7d9d3d1e10ba3697a45893691abcf48d83b4a77d
Krystyna <krystyna@Mac-mini-Alex.local>
2018-06-25 00:46:37 +0300

    create upload-products

--- app/js/init.js
+++ app/js/init.js
@@ -1,23 +1,36 @@
 ;(function($) {
     // Init ion range slider
-    $('#price-range').ionRangeSlider({
-        type: 'double',
-        grid: false,
-        min: 0,
-        max: 1000,
-        from: 0,
-        to: 600,
-        prefix: "$",
-        hide_min_max: true
-    });
+    if ($('#price-range').length) {
+        $('#price-range').ionRangeSlider({
+            type: 'double',
+            grid: false,
+            min: 0,
+            max: 1000,
+            from: 0,
+            to: 600,
+            prefix: "$",
+            hide_min_max: true
+        });
+    }
 
     // Init slider
-    $('.trending-product-carousel').slick({
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        slidesToShow: 4,
-        slidesToScroll: 4
-    });
+    if ($('.trending-product-carousel').length) {
+        $('.trending-product-carousel').slick({
+            arrows: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            slidesToShow: 4,
+            slidesToScroll: 4
+        });
+    }
+
+    // Init form styler
+    if ($('.select-styler, .input-file-styler').length) {
+        $('.select-styler, .input-file-styler').styler({
+            fileBrowse: 'Choose file',
+            filePlaceholder: 'No File Choosen'
+        });
+    }
+
 })(jQuery);
