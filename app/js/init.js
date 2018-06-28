7f17eed2abdd5329c3794179d399ffc67f27c9b7
Krystyna <krystyna@Mac-mini-Alex.local>
2018-06-28 22:52:52 +0300

    fix bugs

--- app/js/init.js
+++ app/js/init.js
@@ -21,7 +21,30 @@
             autoplay: true,
             autoplaySpeed: 5000,
             slidesToShow: 4,
-            slidesToScroll: 4
+            slidesToScroll: 4,
+            responsive: [
+                {
+                  breakpoint: 1200,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                  }
+                },
+                {
+                  breakpoint: 991,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 767,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+            ]
         });
     }
 
