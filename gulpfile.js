ccc268cbb68e920cfdd8b812fe8c8967841a506c
Krystyna <krystyna@Mac-mini-Alex.local>
2018-07-02 12:21:55 +0300

    add concat min css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 // подключение пакетов
 const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer'); // проставляет вендорные префиксы
-const rename = require('gulp-rename'); // переименовывает файлы
-const minifycss = require('gulp-cssmin'); // минимизирует css
-const tinypng = require('gulp-tinypng'); // оптимизирует картинки
+const autoprefixer = require('gulp-autoprefixer'); 
+const rename = require('gulp-rename'); 
+const minifycss = require('gulp-cssmin'); 
+const tinypng = require('gulp-tinypng'); 
 const spritesmith = require('gulp.spritesmith');
 const merge = require('merge-stream');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+// const minifyjs = require('gulp-js-minify');
 
 gulp.task('css', function() {
     return gulp.src('src/css/**/*.css')
@@ -16,14 +19,18 @@ gulp.task('css', function() {
 });
 
 gulp.task('cssmin', function() {
-    return gulp.src('app/css/style.css')
+    return gulp.src([
+        'app/css/**/*.css',
+        '!app/css/style.min.css'
+    ])
+        .pipe(concat('styles.css'))
         .pipe(minifycss())
         .pipe(rename('style.min.css'))
         .pipe(gulp.dest('app/css/'));
 })
 
 gulp.task('tinypng', function() {
-    return gulp.src(['src/img/*.png', 'src/img/*.jpg'])
+    return gulp.src(['app/img/*.png', 'app/img/*.jpg'])
         .pipe(tinypng('eGwHsgRHgRYLa2syNI121TJRmNwb7J46'))
         .pipe(gulp.dest('app/img/'));
 })
@@ -47,6 +54,18 @@ gulp.task('sprite', function() {
         .pipe(gulp.dest('app/css/'));
     return merge(imgStream, cssStream);
 })
+
+gulp.task('js', function() {
+    return gulp.src('app/js/main/**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(concat('scripts.js'))
+        // .pipe(minifyjs())
+        // .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('app/js/'));
+})
+
 gulp.task('watch', ['css', 'cssmin', 'sprite'] ,function() {
     gulp.watch('src/css/**/*.css', ['css']);
     gulp.watch('app/css/style.css', ['cssmin']);
